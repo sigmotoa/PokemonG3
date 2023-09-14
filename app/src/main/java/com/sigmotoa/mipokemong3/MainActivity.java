0b0fe45481cf3cfa2423b230c1a6fb97b6243d78
sigmotoa <11698886+sigmotoa@users.noreply.github.com>
2023-09-14 07:05:20 -0500

    Scroll lateral OK. Pendiente Scroll para las cards

--- app/src/main/java/com/sigmotoa/mipokemong3/MainActivity.java
+++ app/src/main/java/com/sigmotoa/mipokemong3/MainActivity.java
@@ -20,6 +20,27 @@ public class MainActivity extends AppCompatActivity {
         ImageView imgPokemon3 = findViewById(R.id.iv_pokemon_3);
         ImageView imgPokemon4 = findViewById(R.id.iv_pokemon_4);
         ImageView imgPokemon5 = findViewById(R.id.iv_pokemon_5);
+        ImageView imgPokemon6 = findViewById(R.id.iv_pokemon_6);
+        ImageView imgPokemon7 = findViewById(R.id.iv_pokemon_7);
+        ImageView imgPokemon8 = findViewById(R.id.iv_pokemon_8);
+        ImageView imgPokemon9 = findViewById(R.id.iv_pokemon_9);
+        ImageView imgPokemon10 = findViewById(R.id.iv_pokemon_10);
+        ImageView imgPokemon11 = findViewById(R.id.iv_pokemon_11);
+        ImageView imgPokemon12 = findViewById(R.id.iv_pokemon_12);
+        ImageView imgPokemon13 = findViewById(R.id.iv_pokemon_13);
+        ImageView imgPokemon14 = findViewById(R.id.iv_pokemon_14);
+        ImageView imgPokemon15 = findViewById(R.id.iv_pokemon_15);
+        ImageView imgPokemon16 = findViewById(R.id.iv_pokemon_16);
+        ImageView imgPokemon17 = findViewById(R.id.iv_pokemon_17);
+        ImageView imgPokemon18 = findViewById(R.id.iv_pokemon_18);
+        ImageView imgPokemon19 = findViewById(R.id.iv_pokemon_19);
+        ImageView imgPokemon20 = findViewById(R.id.iv_pokemon_20);
+        ImageView imgPokemon21 = findViewById(R.id.iv_pokemon_21);
+        ImageView imgPokemon22 = findViewById(R.id.iv_pokemon_22);
+        ImageView imgPokemon23 = findViewById(R.id.iv_pokemon_23);
+        ImageView imgPokemon24 = findViewById(R.id.iv_pokemon_24);
+        ImageView imgPokemon25 = findViewById(R.id.iv_pokemon_25);
+        ImageView imgPokemon26 = findViewById(R.id.iv_pokemon_26);
 
         Glide.with(this)
                 .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif")
@@ -41,6 +62,90 @@ public class MainActivity extends AppCompatActivity {
                 .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/5.gif")
                 .centerCrop()
                 .into(imgPokemon5);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif")
+                .centerCrop()
+                .into(imgPokemon6);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/7.gif")
+                .centerCrop()
+                .into(imgPokemon7);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/8.gif")
+                .centerCrop()
+                .into(imgPokemon8);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/9.gif")
+                .centerCrop()
+                .into(imgPokemon9);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/10.gif")
+                .centerCrop()
+                .into(imgPokemon10);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/11.gif")
+                .centerCrop()
+                .into(imgPokemon11);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/12.gif")
+                .centerCrop()
+                .into(imgPokemon12);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif")
+                .centerCrop()
+                .into(imgPokemon13);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/4.gif")
+                .centerCrop()
+                .into(imgPokemon14);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/5.gif")
+                .centerCrop()
+                .into(imgPokemon15);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif")
+                .centerCrop()
+                .into(imgPokemon16);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/2.gif")
+                .centerCrop()
+                .into(imgPokemon17);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif")
+                .centerCrop()
+                .into(imgPokemon18);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/4.gif")
+                .centerCrop()
+                .into(imgPokemon19);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/5.gif")
+                .centerCrop()
+                .into(imgPokemon20);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif")
+                .centerCrop()
+                .into(imgPokemon21);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/2.gif")
+                .centerCrop()
+                .into(imgPokemon22);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif")
+                .centerCrop()
+                .into(imgPokemon23);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/4.gif")
+                .centerCrop()
+                .into(imgPokemon24);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/25.gif")
+                .centerCrop()
+                .into(imgPokemon25);
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/26.gif")
+                .centerCrop()
+                .into(imgPokemon26);
 
 
     }
